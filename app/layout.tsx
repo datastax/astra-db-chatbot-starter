cf0c6db34891fcb4d1debd909f4f586f05bd4832
Alex Leventer <3254549+alexleventer@users.noreply.github.com>
2023-11-13 13:11:18 -0800

    Remove getTheme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import { Roboto_Flex } from 'next/font/google'
 import './globals.css'
-import { getTheme } from '../lib/getTheme';
 
 const roboto = Roboto_Flex({ subsets: ['latin'] })
 
@@ -13,7 +12,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <script dangerouslySetInnerHTML={{ __html: getTheme }} />
       </head>
       <body className={roboto.className}>{children}</body>
     </html>
