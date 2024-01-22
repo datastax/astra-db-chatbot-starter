b50052a49bb7035c334f51369f73c161253204f1
ElenaKusevska <elena.kusevska@gmail.com>
2024-01-22 22:28:09 +0100

    add check that dimension is defined and is an integer. Add comments to keyspace collection exists checks

--- populate_db/create_collection.py
+++ populate_db/create_collection.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 from dotenv import load_dotenv
 
@@ -13,11 +14,22 @@ keyspace = os.getenv("ASTRA_DB_KEYSPACE")
 collection_name = os.getenv("ASTRA_DB_COLLECTION_NAME")
 dimension = os.getenv("VECTOR_DIMENSION")
 
+# check that dimension is defined and is an integer
+if dimension == None:
+    print("environment variable 'VECTOR_DIMENSION' not defined")
+    sys.exit()
+else:
+    if not dimension.isdigit():
+        print("environment variable 'VECTOR_DIMENSION' not integer")
+        sys.exit()
+
+# check that keyspace is defined
 if not keyspace:
     astra_db = AstraDB(token=token, api_endpoint=api_endpoint)
 else:
     astra_db = AstraDB(token=token, api_endpoint=api_endpoint, namespace=keyspace)
 
+# create collection if it doesn't exist
 if collection_name in astra_db.get_collections()['status']['collections']:
     print(f"Collection '{collection_name}' already exists. New collection not created")
 else:
